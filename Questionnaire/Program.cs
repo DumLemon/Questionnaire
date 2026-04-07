8f3c5cf78ef1693e7e366e493bfe47295f465160
Scrippt <vasconcelos.aba@gmail.com>
2026-04-07 16:18:19 +0200

    Showing the inputs and creating a Country Input

--- Questionnaire/Program.cs
+++ Questionnaire/Program.cs
@@ -2,12 +2,12 @@
 {
     public class System 
     {
-        public static void Main() 
+        public static void Main()
         {
             Console.ForegroundColor = ConsoleColor.Green;
 
-            
-            string name_input, city_of_birth, day_input, month_input, year_input, city_input;
+
+            string name_input, city_of_birth, day_input, month_input, year_input, city_input, country_input;
             int day, month, year;
 
             #region NameInputs
@@ -15,7 +15,7 @@
             Console.Write("Enter your name: ");
             name_input = Console.ReadLine();
 
-            while (name_input == "") 
+            while (name_input == "")
             {
                 Console.Write("Enter your name: ");
                 name_input = Console.ReadLine();
@@ -26,7 +26,7 @@
 
             #region DayInputs
 
-            Console.Write("Enter the day of your birhtday: ");
+            Console.Write("Enter the day of your birthday: ");
             day_input = Console.ReadLine();
 
             while (!int.TryParse(day_input, out day))
@@ -39,10 +39,10 @@
 
             #region MonthInput
 
-            Console.Write("Enter the number of the month of your birth ex:(February = 02):\n");
+            Console.Write("Enter the number of the month of your birth (ex:February = 02): ");
             month_input = Console.ReadLine();
 
-            while (!int.TryParse(month_input, out month)) 
+            while (!int.TryParse(month_input, out month))
             {
                 Console.Write("Please enter a valid number:");
                 month_input = Console.ReadLine();
@@ -52,11 +52,11 @@
             #endregion
 
             #region YearInput
-            
+
             Console.Write("Enter your birth year:");
             year_input = Console.ReadLine();
 
-            while (!int.TryParse(year_input, out year)) 
+            while (!int.TryParse(year_input, out year))
             {
                 Console.Write("Enter a valid birth year:");
                 year_input = Console.ReadLine();
@@ -66,18 +66,39 @@
 
             #endregion
 
+            #region CountryInput
+
+            Console.Write("Enter your country:");
+            country_input = Console.ReadLine();
+
+            while (country_input == "")
+            {
+                Console.WriteLine("Enter a valid country:");
+                country_input = Console.ReadLine();
+            }
+
+            #endregion
+
             #region CityInput
 
-            Console.Write("Enter the city where you were born:");
+            Console.Write("Enter your city:");
             city_input = Console.ReadLine();
 
-            while (city_input == "") 
+            while (city_input == "")
             {
-                Console.Write("Enter a valid city:");
+                Console.Write("Please enter a valid city:");
                 city_input = Console.ReadLine();
             }
 
 
+            #endregion
+
+            #region ShowingTheInputs
+            
+            Console.WriteLine("\n\nPlease confirm your details:\n");
+            Console.Write("Name: " + name_input + "\n" +
+                          "Birthday: " + day+"." + month_input+"." + year_input+"\n" +                        
+                          "City: " + city_input+", " + country_input + "\n\n\n");
 
             #endregion
 
