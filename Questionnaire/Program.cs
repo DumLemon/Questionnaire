65ddc640e7dd90c5b1bdec5f2ba2a184146751d2
Scrippt <vasconcelos.aba@gmail.com>
2026-04-07 15:18:48 +0200

    Asking for the input day and creating a while to request another answer

--- Questionnaire/Program.cs
+++ Questionnaire/Program.cs
@@ -7,28 +7,36 @@
             Console.ForegroundColor = ConsoleColor.Green;
 
             
-            string name, city_of_birth;
-
-            int day, month, year;
-
+            string name, city_of_birth, day_input, month, year;
+            int day;
 
             #region NameInputs
 
-            Console.Write("Enter your name:");
+            Console.Write("Enter your name: ");
             name = Console.ReadLine();
 
             while (name == "") 
             {
-                Console.Write("Enter your name:");
+                Console.Write("Enter your name: ");
                 name = Console.ReadLine();
 
             }
-            
+
+            #endregion 
+
+
+            #region AgeInputs
+
+            Console.Write("Enter the day of your birhtday: ");
+            day_input = Console.ReadLine();
+
+            while (!int.TryParse(day_input, out day))
+            {
+                Console.Write("Please enter a valid number:");
+                day_input = Console.ReadLine();
+            }
+
             #endregion
-
-
-
-
 
 
         }
