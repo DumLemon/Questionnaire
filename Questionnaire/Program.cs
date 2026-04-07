f7bbcef01364f627bd5fe8df7426c99a6b930a9f
Scrippt <vasconcelos.aba@gmail.com>
2026-04-07 14:30:38 +0200

    Input name

--- Questionnaire/Program.cs
+++ Questionnaire/Program.cs
@@ -4,6 +4,28 @@
     {
         public static void Main() 
         {
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            
+            string name, city_of_birth;
+
+            int day, month, year;
+
+
+            #region NameInputs
+
+            Console.Write("Enter your name:");
+            name = Console.ReadLine();
+
+            while (name == "") 
+            {
+                Console.Write("Enter your name:");
+                name = Console.ReadLine();
+
+            }
+            
+            #endregion
+
 
 
 
@@ -13,7 +35,11 @@
     }
 
 
-
+    //name
+    //Date of birth(age)
+    //City of birth
+    
+    
 
 
 
